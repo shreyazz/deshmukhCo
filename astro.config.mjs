2eb71ec03a0dd87f606c9eb00f3ebf6180fe230c
Shreyas Pahune <pahuneshreyas@gmail.com>
2022-12-18 01:38:08 +0530

    initial commit ✨

---
+++ astro.config.mjs
@@ -0,0 +1,21 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+import compress from "astro-compress";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), compress(
+    {
+      html: true,
+      css: true,
+      js: true,
+      img: true,
+      svg: true,
+      path: './dist'
+    }
+  )]
+});
