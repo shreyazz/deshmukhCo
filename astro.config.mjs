8a46154a7f1dbcbdff797ec83b309b43f28da589
Shreyas Pahune <pahuneshreyas@gmail.com>
2022-12-18 02:07:30 +0530

    feat: added astro/sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
@@ -6,16 +6,21 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 import compress from "astro-compress";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), compress(
-    {
+  site: " http://localhost:3000/",
+  integrations: [
+    tailwind(),
+    sitemap(),
+    compress({
       html: true,
       css: true,
       js: true,
       img: true,
       svg: true,
-      path: './dist'
-    }
-  )]
+      path: "./dist",
+    }),
+  ],
 });
