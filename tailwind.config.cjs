52ec7fc9d08c76118ee34a4c426060ef29bcadc6
Shreyas Pahune <pahuneshreyas@gmail.com>
2022-12-29 23:06:41 +0530

    added tailwind css

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,17 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		fontFamily: {
+			'sans': ['"Rubik"', 'sans-serif'],
+			'serif': ['"Cormorant"', 'serif'],
+		  },
+		  colors: {
+			'darkBlue': '#132958',
+			'gold': '#D0A354',
+			'offWhite': '#F5F5F5',
+			'pureWhite': "#fff",
+			'black': '#1B1C1E',
+		  },
 	},
 	plugins: [],
 }
